6f087cc544217ab0afd6d290e3922fb7f2d83960
Jeff Tarr <jeff@tarr.com>
2021-10-22 10:06:55 -0400

    Lutron_Button_Press.sh
    initial commit

---
+++ Lutron_Button_Press.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# From Lutron's software download the Integration report to get device #s and button #s
+# To simulate a Keypad button press the command is #DEVICE,<keypad #>,<button #>,3
+
+#Jeff's Office, Device 48, Button 1 = High, Button 6 = Off  -- #DEVICE,48,1,3 to activate "HIGH"
+#Jeff's Office, Device 50, Button 1 = Solar, Button 2 = Blackout, Button 6=Goodnight
+
+# UPDATE IP ADDRESS BELOW TO YOUR LUTRON PROCESSOR
+# UPDATE USERNAME TO YOUR USERNAME (below it is lutron)
+# UPDATE PASSWORD TO YOUR PASSWORD (below it is integration)
+# UPDATE LINE #23 TO YOUR DEVICE ID AND BUTTON ID
+
+/opt/homebrew/bin/expect -c '
+log_file -a /tmp/status.log
+set timeout 20
+spawn /opt/homebrew/bin/telnet 10.0.1.200
+expect "login: " 
+send "lutron\r"
+expect "password: "
+send "integration\r"
+expect "GNET> "
+send "#DEVICE,48,6,3\r"
+expect "GNET> "
+exit
+' 
+exit
+
